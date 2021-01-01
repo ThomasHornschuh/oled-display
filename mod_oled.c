a020ba199c705867514a381d264ea48eba89f515
Thomas Hornschuh <thomas@hornschuh-family.de>
2021-01-02 00:32:12 +0100

    Progress in lua mod

--- mod_oled.c
+++ mod_oled.c
@@ -32,7 +32,15 @@ static int mod_oled_clrscr( lua_State* L )
 
 static int mod_oled_gotoxy( lua_State* L )
 {
-    lcd_gotoxy(_lua_checkbyte( L, 1), _lua_checkbyte( L, 2) );
+    uint8_t x = _lua_checkbyte( L, 1);
+    uint8_t y = _lua_checkbyte( L, 2);
+    int pixmode = luaL_optint(L,3,0);
+
+    if (pixmode) 
+       lcd_goto_xpix_y( x,y );
+    else   
+        lcd_gotoxy( x,y );
+
     return 0;
 }
 
@@ -40,6 +48,17 @@ static int mod_oled_writetext(  lua_State* L )
 {
     const char * str = luaL_checkstring(L,1);
     lcd_puts(str);   
+    return 0;
+}
+
+static int mod_oled_charheight(  lua_State* L )
+{
+    int v = luaL_checkinteger( L, 1 );
+    luaL_argcheck( L, (v==NORMALSIZE || v==DOUBLESIZE) ,1,  
+       "expect either oled.NORMALSIZE or oled.DOUBLESIZE" );
+
+    lcd_charMode( v );   
+
     return 0;
 }
 
@@ -56,7 +75,7 @@ static int mod_oled_rect(  lua_State* L  )
    uint8_t py1 = _lua_checkbyte( L, 2 );
    uint8_t px2 = _lua_checkbyte( L, 3 );
    uint8_t py2 = _lua_checkbyte( L, 4 );
-   uint8_t color = _lua_checkbyte( L, 5 );
+   uint8_t color = (_lua_checkbyte( L, 5 )==BLACK)?BLACK:WHITE;
    int     fill = luaL_optint( L, 6, 0 );
 
    if (fill)
@@ -65,8 +84,37 @@ static int mod_oled_rect(  lua_State* L  )
        lcd_drawRect(px1,py1,px2,py2,color); 
         
     return 0;
-
 }
+
+static int mod_oled_line(  lua_State* L  )
+{
+   uint8_t px1 = _lua_checkbyte( L, 1 );
+   uint8_t py1 = _lua_checkbyte( L, 2 );
+   uint8_t px2 = _lua_checkbyte( L, 3 );
+   uint8_t py2 = _lua_checkbyte( L, 4 );
+   uint8_t color = (_lua_checkbyte( L, 5 )==BLACK)?BLACK:WHITE;
+  
+   lcd_drawLine(px1,py1,px2,py2,color); 
+        
+    return 0;
+}
+
+static int mod_oled_circle(  lua_State* L  )
+{
+   uint8_t center_x = _lua_checkbyte( L, 1 );
+   uint8_t center_y = _lua_checkbyte( L, 2 );
+   uint8_t radius = _lua_checkbyte( L, 3 );
+   uint8_t color = (_lua_checkbyte( L, 4 )==BLACK)?BLACK:WHITE;
+   int     fill = luaL_optint( L, 5, 0 );
+
+   if (fill)
+       lcd_fillCircle(center_x,center_y,radius,color);
+    else
+        lcd_drawCircle(center_x,center_y,radius,color); 
+        
+    return 0;
+}
+
 
 #endif
 
@@ -79,12 +127,25 @@ const LUA_REG_TYPE oled_map[] =
   { LSTRKEY( "clrscr" ), LFUNCVAL( mod_oled_clrscr ) },
   { LSTRKEY( "gotoxy" ), LFUNCVAL( mod_oled_gotoxy ) },
   { LSTRKEY( "writetext" ), LFUNCVAL( mod_oled_writetext ) },
+  { LSTRKEY( "charheight" ), LFUNCVAL( mod_oled_charheight ) },
   #ifdef GRAPHICMODE
    { LSTRKEY( "display" ), LFUNCVAL( mod_oled_display ) },
    { LSTRKEY( "rect" ), LFUNCVAL( mod_oled_rect ) },
+   { LSTRKEY( "line" ), LFUNCVAL( mod_oled_line ) },
+   { LSTRKEY( "circle" ), LFUNCVAL( mod_oled_circle ) },
   #endif
+  
   { LSTRKEY( "FILL" ), LNUMVAL( 1 ) },
   { LSTRKEY( "NO_FILL" ), LNUMVAL( 0 ) },
+  { LSTRKEY( "PIX_MODE" ), LNUMVAL( 1 ) },
+  { LSTRKEY( "CHAR_MODE" ), LNUMVAL( 0 ) },
+  { LSTRKEY( "WHITE" ), LNUMVAL( WHITE ) },
+  { LSTRKEY( "BLACK" ), LNUMVAL( BLACK ) },
+  { LSTRKEY( "DISPLAY_WIDTH" ), LNUMVAL( DISPLAY_WIDTH ) },
+  { LSTRKEY( "DISPLAY_HEIGHT" ), LNUMVAL( DISPLAY_HEIGHT ) }, 
+  { LSTRKEY( "DOUBLESIZE" ), LNUMVAL( DOUBLESIZE ) },
+  { LSTRKEY( "NORMALSIZE" ), LNUMVAL( NORMALSIZE ) },
+  
   //{ LSTRKEY( "debugger" ), LFUNCVAL( debugger ) },
 
 
